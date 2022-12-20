d285a9c60c1ff81b9e1e49f55211a81edefa875f
DevGibby <71801512+DevGibby@users.noreply.github.com>
2022-12-20 08:23:52 -0500

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,7 +9,7 @@ export const Nav = () => {
   return (
     <StyledNav>
       <Link to="/">Football Weather</Link>
-      <h2>Week 15</h2>
+      <h2>Week 16</h2>
       <h3>Kickoff Forecasts</h3>
     </StyledNav>
   )
