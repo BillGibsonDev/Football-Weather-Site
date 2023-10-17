8dd71d0a810930dd8daad59e88cf30a1890b41b7
Bill Gibson <71801512+BillGibsonDev@users.noreply.github.com>
2023-10-17 17:26:02 -0400

    more meta tags for helmet

--- src/components/HelmetComponent.js
+++ src/components/HelmetComponent.js
@@ -6,6 +6,12 @@ export const HelmetComponent = ({ game }) => {
     <Helmet>
       <title>{`Week ${game.Week} - ${game.AwayTeam} @ ${game.HomeTeam} Forecast | 4Cast4Football`}</title>
       <meta name="description" content={`Week ${game.Week} - ${game.AwayTeam} @ ${game.HomeTeam} Forecast`} />
+      <meta property="og:title" content={`Week ${game.Week} - ${game.AwayTeam} @ ${game.HomeTeam} Forecast | 4Cast4Football`} />
+      <meta property="og:description" content={`Week ${game.Week} - ${game.AwayTeam} @ ${game.HomeTeam} Forecast`} />
+      <meta property="og:image" content="../src/assets/patrick-ogilvie-GB9XKDZWwp0-unsplash(1).jpg" />
+      <meta property="og:url" content="https://www.4cast4football.com" />
+      <meta property="og:type" content="website" />
+      {/* Twitter Tags */}
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@4Cast4Football" />
       <meta name="twitter:creator" content="@4Cast4Football" />
