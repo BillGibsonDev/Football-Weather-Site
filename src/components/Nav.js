b292c16cf5f92ea227bc6363d98a3540981b1c88
Bill Gibson <71801512+BillGibsonDev@users.noreply.github.com>
2023-12-12 09:21:21 -0500

    Update Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,7 +10,7 @@ export const Nav = () => {
     <StyledNav>
       <Link to="/">Football Forecast</Link>
       <h2>2023 Season</h2>
-      <h2>Week 14</h2>
+      <h2>Week 15</h2>
     </StyledNav>
   )
 }
