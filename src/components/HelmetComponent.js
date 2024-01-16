9f9162234c61db51e6568accafea9082a8d9dd58
Bill Gibson <71801512+BillGibsonDev@users.noreply.github.com>
2024-01-16 09:30:25 -0500

    weekly update

--- src/components/HelmetComponent.js
+++ src/components/HelmetComponent.js
@@ -6,10 +6,10 @@ export const HelmetComponent = ({ game, home }) => {
   if(home && game){
     return (
       <Helmet>
-        <title>{`Wildcard Weekend  Forecast | 4Cast4Football`}</title>
-        <meta name="description" content={`Wildcard Weekend Forecast`} />
-        <meta property="og:title" content={`Wildcard Weekend Forecast | 4Cast4Football`} />
-        <meta property="og:description" content={`Wildcard Weekend Forecast`} />
+        <title>{`Divisonal Round Forecast | 4Cast4Football`}</title>
+        <meta name="description" content={`Divisonal Round Forecast`} />
+        <meta property="og:title" content={`Divisonal Round Forecast | 4Cast4Football`} />
+        <meta property="og:description" content={`Divisonal Round Forecast`} />
         <meta property="og:image" content={FieldImage} />
         <meta property="og:url" content="https://www.4cast4football.com" />
         <meta property="og:type" content="website" />
@@ -17,8 +17,8 @@ export const HelmetComponent = ({ game, home }) => {
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:site" content="@4Cast4Football" />
         <meta name="twitter:creator" content="@4Cast4Football" />
-        <meta name="twitter:title" content={`Wildcard Weekend Forecast`} />
-        <meta name="twitter:description" content={`Wildcard Weekend Forecast`} />
+        <meta name="twitter:title" content={`Divisonal Round Forecast`} />
+        <meta name="twitter:description" content={`Divisonal Round Forecast`} />
         <meta name="twitter:image" content={FieldImage} />
     </Helmet>
     );
@@ -26,10 +26,10 @@ export const HelmetComponent = ({ game, home }) => {
 
   return (
     <Helmet>
-      <title>{`Week ${game.Week} - ${game.AwayTeam} @ ${game.HomeTeam} Forecast | 4Cast4Football`}</title>
-      <meta name="description" content={`Week ${game.Week} - ${game.AwayTeam} @ ${game.HomeTeam} Forecast`} />
-      <meta property="og:title" content={`Week ${game.Week} - ${game.AwayTeam} @ ${game.HomeTeam} Forecast | 4Cast4Football`} />
-      <meta property="og:description" content={`Week ${game.Week} - ${game.AwayTeam} @ ${game.HomeTeam} Forecast`} />
+      <title>{`Divisonal Round - ${game.AwayTeam} @ ${game.HomeTeam} Forecast | 4Cast4Football`}</title>
+      <meta name="description" content={`Divisonal Round - ${game.AwayTeam} @ ${game.HomeTeam} Forecast`} />
+      <meta property="og:title" content={`Divisonal Round - ${game.AwayTeam} @ ${game.HomeTeam} Forecast | 4Cast4Football`} />
+      <meta property="og:description" content={`Divisonal Round - ${game.AwayTeam} @ ${game.HomeTeam} Forecast`} />
       <meta property="og:image" content={FieldImage} />
       <meta property="og:url" content="https://www.4cast4football.com" />
       <meta property="og:type" content="website" />
@@ -37,8 +37,8 @@ export const HelmetComponent = ({ game, home }) => {
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:site" content="@4Cast4Football" />
       <meta name="twitter:creator" content="@4Cast4Football" />
-      <meta name="twitter:title" content={`Week ${game.Week} - ${game.AwayTeam} @ ${game.HomeTeam} Forecast`} />
-      <meta name="twitter:description" content={`Week ${game.Week} - ${game.AwayTeam} @ ${game.HomeTeam} Forecast`} />
+      <meta name="twitter:title" content={`Divisonal Round - ${game.AwayTeam} @ ${game.HomeTeam} Forecast`} />
+      <meta name="twitter:description" content={`Divisonal Round - ${game.AwayTeam} @ ${game.HomeTeam} Forecast`} />
       <meta name="twitter:image" content={FieldImage} />
     </Helmet>
   );
