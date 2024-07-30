5c8cd6bcbedda3bcb2cdd6608af2e74b1b0f3a13
Bill Gibson <71801512+BillGibsonDev@users.noreply.github.com>
2024-07-29 21:55:20 -0400

    Revert "Update Weather.js"
    This reverts commit 1e3a731b0cd52f9a2ac62a8395f344e19ae3bbe9.

--- src/pages/Home/components/Weather.js
+++ src/pages/Home/components/Weather.js
@@ -6,16 +6,18 @@ export const Weather = ({weather}) => {
 
     return (
         <StyledWeather>
-            <div className="top-condition-container">
-                <img src={weather.icon} alt="" />
-                <div className="icon-text-container">
-                    <h5>{weather.temperature}<span>&deg;</span></h5>
-                    <h4>{weather.shortForecast}</h4>
+            <div className="weather-wrapper">
+                <div className="top-condition-container">
+                    <img src={weather.icon} alt="" />
+                    <div className="icon-text-container">
+                        <h5>{weather.temperature}<span>&deg;</span></h5>
+                        <h4>{weather.shortForecast}</h4>
+                    </div>
                 </div>
-            </div>
-            <div className="bottom-condition-container">
-                <h6><span className="title">Wind</span>{weather.windSpeed}</h6>
-                <h6><span className="percip">Precipitation</span>{Math.floor(weather.probabilityOfPrecipitation.value)}<span className="unit">%</span></h6>
+                <div className="bottom-condition-container">
+                    <h6><span className="title">Wind</span>{weather.windSpeed}</h6>
+                    <h6><span className="percip">Precipitation</span>{Math.floor(weather.probabilityOfPrecipitation.value)}<span className="unit">%</span></h6>
+                </div>
             </div>
         </StyledWeather>
     )
@@ -26,50 +28,53 @@ const StyledWeather = styled.article`
     justify-content: center;
     align-items: center;
     margin-bottom: 10px;
-    .top-condition-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 0 auto;
-        img {
-            width: 50px;
-            height: 50px;
-            border-radius: 50%;
-            margin: 0 20px auto 0;
-        }
-        .icon-text-container {
-            max-width: 250px;
-            width: 70%;
-            h4 {
-                font-size: ${palette.labelSize};
-                color: ${palette.labelColor};
-                display: flex;
-                font-weight: 400;
-                width: 100%;
-            }
-            h5 {
-                margin-bottom: 6px;
-                font-size: ${palette.subtitleSize};
-                font-weight: 600;
-            }
-        }
-    }
-    .bottom-condition-container {
-        margin: 10px auto;
-        width: 90%;
-        h6 {
-            margin: 2px 0;
-            font-size: ${palette.smallTextSize};
+    .weather-wrapper {
+        .top-condition-container {
             display: flex;
-            justify-content: space-between;
-            min-width: 150px;
-            font-weight: 400;
-            color: ${palette.labelColor};
-            .wind, .percip {
-                margin-right: auto;
+            justify-content: center;
+            align-items: center;
+            margin: 0 auto;
+            img {
+                width: 50px;
+                height: 50px;
+                border-radius: 50%;
+                margin: 0 20px auto 0;
             }
-            span {
-                font-weight: 200;
+            .icon-text-container {
+                max-width: 250px;
+                width: 70%;
+                h4 {
+                    font-size: ${palette.labelSize};
+                    color: ${palette.labelColor};
+                    display: flex;
+                    font-weight: 400;
+                    width: 100%;
+                }
+                h5 {
+                    margin-bottom: 6px;
+                    font-size: ${palette.subtitleSize};
+                    font-weight: 600;
+                    color: ${palette.labelColor};
+                }
+            }
+        }
+        .bottom-condition-container {
+            margin: 10px auto;
+            width: 90%;
+            h6 {
+                margin: 2px 0;
+                font-size: ${palette.smallTextSize};
+                display: flex;
+                justify-content: space-between;
+                min-width: 150px;
+                font-weight: 400;
+                color: ${palette.labelColor};
+                .wind, .percip {
+                    margin-right: auto;
+                }
+                span {
+                    font-weight: 200;
+                }
             }
         }
     }
